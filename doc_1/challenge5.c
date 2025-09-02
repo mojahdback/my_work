d123301a46030f07fa474b9a4e78be00c158f76c
mohamed <mojahdontheway@gmail.com>
2025-09-02 14:23:50 +0200

    new edit

--- doc_1/challenge5.c
+++ doc_1/challenge5.c
@@ -6,7 +6,6 @@ int main(){
 
    printf("Enter X: ");
    scanf("%d",&x);
-   // 3x5+2x4–5x3–x2+7x–6 
    int sum = (3*x*x*x*x*x) + (5*x*x*x*x) - (x*x) + (7*x) - 6;
 
    printf("la valeur du polynome : %d",sum);
